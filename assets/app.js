54abda12e87937cf7cc8e2db88cde1ac02ed5ec2
Catraco <93885462+catraco@users.noreply.github.com>
2022-12-26 23:50:09 +0100

    add loading page

--- assets/app.js
+++ assets/app.js
@@ -5,3 +5,19 @@
 -------------------
 
 */
+
+function setup(){
+
+	// Setup function //
+	
+	setTimeout(() => {
+		document.getElementsByClassName("loading-page")[0].style.display = "none";
+	}, 2000);
+
+}
+
+window.onload = () => {
+
+	setup();
+
+}
